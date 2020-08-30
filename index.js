e6fac7005ba73096d5e7d9ddf1628355a441cd67
rajVashishtha <vashiraj2000@gmail.com>
2020-08-30 12:43:36 +0530

    Intro page added and some changes in directory

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express()
 app.use(express.static("./static"))
 
 app.get("/",(req,res)=>{
+    res.sendFile("index.html",{root: __dirname})
+})
+app.get("/sort",(req,res)=>{
     res.sendFile("index_sort.html",{root: __dirname})
 })
 app.listen(PORT,()=>{
