eb1667be81db1a87ed3ca23de698b5d711aa11e6
rajVashishtha <vashiraj2000@gmail.com>
2025-06-29 22:11:25 +0530

    Finished serverless configuration

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const express = require('express')
-const PORT = process.env.PORT || 5001
+const express = require('express');
+const serverless = require('serverless-http');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-app.use(express.static("./static"))
+// Serve static files from the "static" directory
+app.use(express.static(path.join(__dirname, 'static')));
 
-app.get("/",(req,res)=>{
-    res.sendFile("index.html",{root: __dirname})
-})
-app.get("/sort",(req,res)=>{
-    res.sendFile("index_sort.html",{root: __dirname})
-})
-app.get("/search",(req,res)=>{
-    res.sendFile("index_search.html",{root: __dirname})
-})
-app.listen(PORT,()=>{
-    console.log("running")
-})
+// Route: Home
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+// Route: Sort Page
+app.get('/sort', (req, res) => {
+  res.sendFile('index_sort.html', { root: __dirname });
+});
+
+// Route: Search Page
+app.get('/search', (req, res) => {
+  res.sendFile('index_search.html', { root: __dirname });
+});
+
+// Only listen when running locally (not on Vercel)
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Local server running on port ${PORT}`);
+  });
+}
+
+// Export for Vercel (as serverless function)
+module.exports = app;
+module.exports.handler = serverless(app);
